428701d93c2f22cacfdec1f3d8fed9af812883d7
aslpavel <asl.pavel@gmail.com>
2012-09-21 17:07:32 +0400

    [core] fixed: cleaned up thread_pool from exportes

--- core/__init__.py
+++ core/__init__.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
-from . import core, file, sock, thread_pool
+from . import core, file, sock
 
 from .core import *
 from .file import *
 from .sock import *
-from .thread_pool import *
 
-__all__ = core.__all__ + file.__all__ + sock.__all__ + thread_pool.__all__
+__all__ = core.__all__ + file.__all__ + sock.__all__
 # vim: nu ft=python columns=120 :
